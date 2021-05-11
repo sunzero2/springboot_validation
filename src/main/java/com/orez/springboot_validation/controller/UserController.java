cc87975b948c4b8ae5d868714494cda918d39172
ngle-hyeyeong <sun_zero2@naver.com>
2021-05-12 00:17:26 +0900

    lombok annotationProcessor 추가

--- src/main/java/com/orez/springboot_validation/controller/UserController.java
+++ src/main/java/com/orez/springboot_validation/controller/UserController.java
@@ -1,23 +1,21 @@
 package com.orez.springboot_validation.controller;
 
 import com.orez.springboot_validation.model.User;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @PostMapping("/save")
-    public ResponseEntity saveUser(@RequestBody Map<String, Object> user, BindingResult bindingResult) {
+    public ResponseEntity saveUser(@RequestBody @Valid User user, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
 
         return ResponseEntity.ok(user);
